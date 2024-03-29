4c87f2df9fafce73babd12393c5dc686a269dd74
Jelle Meeus <jellemeeus@hotmail.com>
2024-03-29 03:46:48 +0100

    save

--- electron-vite-vue/src/App.vue
+++ electron-vite-vue/src/App.vue
@@ -26,17 +26,6 @@ type Data = {
 </script>
 
 <template>
-  <div>
-    <a href="https://www.electronjs.org/" target="_blank">
-      <img src="./assets/electron.svg" class="logo electron" alt="Electron logo" />
-    </a>
-    <a href="https://vitejs.dev/" target="_blank">
-      <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <HelloWorld msg="Electron + Vite + Vue" />
   <div class="content">
     <div class="row clips-and-compilation">
